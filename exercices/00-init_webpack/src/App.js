b9f6efee890e150b78e439904ba85bc18acb64df
Clément Devos <clement@welovedevs.com>
2022-02-01 18:13:41 +0100

    init

---
+++ exercices/00-init_webpack/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Form from './Form';
+// import SVG from './W3D_logo.svg';
+
+function App() {
+    return (
+        <div>
+            <h1>Welcome to React App</h1>
+            <h3>Date : {new Date().toDateString()}</h3>
+            {/*<img src={SVG} />*/}
+            <Form />
+        </div>
+    );
+}
+
+export default App;
